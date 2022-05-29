0b86b0033f24580ef87541a2ab46d980ca35c39e
beibei.luo01 <beibei.luo01@weimob.com>
2022-05-29 22:14:38 +0800

    fix: bug

--- packages/cz/index.js
+++ packages/cz/index.js
@@ -4,6 +4,7 @@ module.exports = {
   prompter: function (cz, commit) {
     options.prompter(cz, function (...params) {
       commit(...params)
+      console.log('commit exec')
       process.exit(1)
     })
   }
